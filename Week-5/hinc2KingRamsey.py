a80c411f866ff8dce6d84dcfea9fb20e4408afec
ramsey-king <75282693+ramsey-king@users.noreply.github.com>
2021-04-29 12:34:59 -0500

    Add files via upload

--- Week-5/hinc2KingRamsey.py
+++ Week-5/hinc2KingRamsey.py
@@ -47,23 +47,75 @@ def InterpolateSample(df, log_upper=6.0):
     return log_sample
 
 
+def Median(xs):
+    cdf = thinkstats2.MakeCdfFromList(xs)
+    return cdf.Value(0.5)
+
+
+def PearsonMedianSkewness(xs):
+    pass
+    # median = Median(xs)
+    # mean =
+
+
 def main():
     df = hinc.ReadData()
     log_sample = InterpolateSample(df, log_upper=6.0)
 
     log_cdf = thinkstats2.Cdf(log_sample)
+
     thinkplot.Cdf(log_cdf)
     thinkplot.Show(xlabel='household income',
                    ylabel='CDF')
 
-    # print(log_sample)
-    # print(len(log_sample))
-    # print(df)
-    # data = df.income.dropna()
-    # pdf = thinkstats2.EstimatedPdf(data)
-    # print(pdf)
-
     cdf = thinkstats2.MakeCdfFromList(log_cdf)
-    print(cdf.Value(0.5))
+
+    tsmedian = thinkstats2.Median(log_sample)
+    print("Thinkstats median:", tsmedian)
+    print("Thinkstats median converted back to dollars:", "${:,.2f}".format(10 ** tsmedian))
+
+    tsmean = thinkstats2.Mean(log_sample)
+    print("Thinkstats mean:", tsmean)
+
+    print("Thinkstats mean converted back to dollars:", "${:,.2f}".format(10 ** tsmean))
+
+    tsskewness = thinkstats2.Skewness(log_sample)
+    print("Thinkstats skewness:", tsskewness)
+
+    tsPearskewness = thinkstats2.PearsonMedianSkewness(log_sample)
+    print("Thinkstats Pearson's skewness:", tsPearskewness)
+
+    pdf = thinkstats2.EstimatedPdf(log_sample)
+    thinkplot.Pdf(pdf, label='household income')
+    thinkplot.Show(xlabel='household income', ylabel='PDF')
+
+    print("The fraction of households below the mean is: approximately 45.06%.  This is calculated by showing the "
+          "following:")
+    print("The difference between the cdf Value at this percentage and the mean is:", cdf.Value(0.450603472) - tsmean)
+
+    log_sample2 = InterpolateSample(df, log_upper=7.0)
+    log_cdf2 = thinkstats2.Cdf(log_sample2)
+
+    thinkplot.Cdf(log_cdf2)
+    thinkplot.Show(xlabel='household income',
+                   ylabel='CDF')
+
+    tsmean2 = thinkstats2.Mean(log_sample2)
+    print("Thinkstats mean:", tsmean2)
+
+    print("Thinkstats mean converted back to dollars:", "${:,.2f}".format(10 ** tsmean2))
+
+    tsskewness2 = thinkstats2.Skewness(log_sample2)
+
+    tsPearskewness2 = thinkstats2.PearsonMedianSkewness(log_sample2)
+
+    print("If we changed the upper bound, to say 7 or $10 million, the difference in mean in dollars is:",
+          "${:,.2f}".format(10 ** tsmean2 - 10 ** tsmean))
+
+    print("The difference in skewness:", tsskewness2 - tsskewness)
+
+    print("The difference in Pearson's skewness:", tsPearskewness2 - tsPearskewness)
+
+
 if __name__ == "__main__":
     main()
